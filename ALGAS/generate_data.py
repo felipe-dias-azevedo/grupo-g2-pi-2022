4f25f3123570723f349a31ba517ea9b2383afd04
gabrielronny <gabrielronny123@hotmail.com>
2022-02-25 22:51:52 -0300

    correção ao gerar os dados

--- ALGAS/generate_data.py
+++ ALGAS/generate_data.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 from sys import getsizeof
+from datetime import datetime
 
 def generate_data(sort: bool = False):
     dados = []
@@ -8,18 +8,19 @@ def generate_data(sort: bool = False):
         memory_accumulator = 0
         for d in data:
             for val in d:
+                dt_inicio = datetime.now()
+                acumulador= 0
+                for i in range(val, 0, -1):
+                    acumulador += i 
+                    #memory_accumulator += getsizeof(i)
                 memory_accumulator += getsizeof(val)
-                dados.append((val, memory_accumulator, datetime.now()))
+                dados.append((val, memory_accumulator, (datetime.now() - dt_inicio).total_seconds()))
 
     append_data(
-        (range(100000, 600000, 100000), 
-        range(1000, 6000, 100), 
-        range(100, 600, 100), 
-        range(10, 60, 10), 
-        range(1000000, 6000000, 1000000))
+        (range(0, 100000, 100),)
     )
 
     if sort:
         dados = sorted(dados, key=lambda x: x[0])
-    
-    return dados
+
+    return dados 
