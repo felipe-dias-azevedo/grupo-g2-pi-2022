ba5473a23b131765fc2526eb3269e72c474ccb56
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2022-05-28 19:00:42 -0300

    read data to df

--- PYSPARK-SPRINT-3/processing.py
+++ PYSPARK-SPRINT-3/processing.py
@@ -1,2 +1,11 @@
+from datetime import date, datetime
+
 class Process:
-    pass
+    
+    def get_age_from_birthdate(self, birthdate: str) -> int:
+        born = datetime.strptime(birthdate, '%d/%m/%Y')
+        today = date.today()
+        return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+    def get_label_from_floatbool(self, label: float) -> str:
+        return "Sim" if label >= 1 else "Não"
