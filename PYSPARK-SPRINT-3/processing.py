26f8900d4a1260c8cc759d7ed309a1c8380239e1
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2022-05-29 13:20:05 -0300

    processing data

--- PYSPARK-SPRINT-3/processing.py
+++ PYSPARK-SPRINT-3/processing.py
@@ -1,8 +1,26 @@
+import unicodedata
 from datetime import date, datetime
 from difflib import SequenceMatcher
 
 
 class Process:
+    def no_accents_lower(self, text: str):
+        noaccent = self.remove_special_chars(text)
+        return noaccent.lower()
+    
+    def remove_special_chars(self, text: str):
+        try:
+            text = unicode(text, 'utf-8')
+        except NameError:
+            pass
+        text = unicodedata.normalize('NFD', text) \
+            .encode('ascii', 'ignore') \
+            .decode("utf-8")
+        return str(text)
+    
+    def get_date(datestr: str):
+        return datetime.strptime(datestr[:-5], "%Y-%m-%dT%H:%M:%S")
+    
     def is_similar(self, string_a: str, string_b: str, percentage=0.8) -> bool:
         sm = SequenceMatcher(a=string_a, b=string_b)
         return sm.ratio() >= percentage
