3e0ef8cb38fca119776ec4fedf9e7e9f2ca6191c
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2022-05-29 00:02:32 -0300

    processing updates

--- PYSPARK-SPRINT-3/processing.py
+++ PYSPARK-SPRINT-3/processing.py
@@ -1,7 +1,12 @@
 from datetime import date, datetime
+from difflib import SequenceMatcher
+
 
 class Process:
-    
+    def is_similar(self, string_a: str, string_b: str, percentage=0.8) -> bool:
+        sm = SequenceMatcher(a=string_a, b=string_b)
+        return sm.ratio() >= percentage
+
     def get_age_from_birthdate(self, birthdate: str) -> int:
         born = datetime.strptime(birthdate, '%d/%m/%Y')
         today = date.today()
@@ -9,3 +14,9 @@ class Process:
 
     def get_label_from_floatbool(self, label: float) -> str:
         return "Sim" if label >= 1 else "Não"
+
+
+if __name__ == "__main__":
+    p = Process()
+    print(p.is_similar("S@o Pa*lo", "São Paulo"))
+    print(p.is_similar("S@o Paulo", "São Paulo"))
