955c19fbb96e12e74c1d7b749399ba54f222cb9d
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2022-05-30 01:05:36 -0300

    processing tweets data

--- PYSPARK-SPRINT-3/processing.py
+++ PYSPARK-SPRINT-3/processing.py
@@ -2,8 +2,29 @@ import unicodedata
 from datetime import date, datetime
 from difflib import SequenceMatcher
 
+from pandas import DataFrame
+
 
 class Process:
+    def words_in_tweet(self, tweet: list):
+        only_words_tweets = []
+        for words in tweet:
+            onlywords = []
+            for word in words:
+                onlywords.append(word[0])
+            only_words_tweets.append(onlywords)
+        return only_words_tweets
+
+    def freq_tweet(self, tweets: DataFrame):
+        frequency = []
+        for tweet in tweets:
+            count = tweet[1]
+            for t in tweets:
+                if t[0] == tweet[0]:
+                    count += t[1]
+            frequency.append([tweet[0], count])
+        return frequency
+
     def no_accents_lower(self, text: str):
         noaccent = self.remove_special_chars(text)
         return noaccent.lower()
